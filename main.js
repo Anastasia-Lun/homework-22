703877df5a8a60e85e1c50a1b8bd036b20f2c613
Anastasia-Lun <anastasiabtv21@gmail.com>
2025-12-12 23:16:46 +0400

    fixed homework

--- main.js
+++ main.js
@@ -17,17 +17,17 @@ const counter = (function () {
   };
 })();
 
-// console.log(counter()) // 0
-// console.log(counter()) // 1
-// console.log(counter(100)) // 100
-// console.log(counter()) // 101
-// console.log(counter()) // 102
-// console.log(counter(500)) // 500
-// console.log(counter()) // 501
-// console.log(counter()) // 502
-// console.log(counter(0)) // 0
-// console.log(counter()) // 0
-// console.log(counter()) // 1
+console.log(counter()); // 0
+console.log(counter()); // 1
+console.log(counter(100)); // 100
+console.log(counter()); // 101
+console.log(counter()); // 102
+console.log(counter(500)); // 500
+console.log(counter()); // 501
+console.log(counter()); // 502
+console.log(counter(0)); // 0
+console.log(counter()); // 0
+console.log(counter()); // 1
 
 /*
  * #2
@@ -53,20 +53,20 @@ const counterFactory = (function () {
   };
 })();
 
-// console.log(counterFactory.value()) // 0
-// counterFactory.increment()
-// counterFactory.increment()
-// counterFactory.increment()
-// console.log(counterFactory.value()) // 3
-// counterFactory.decrement()
-// counterFactory.decrement()
-// console.log(counterFactory.value()) // 1
-// console.log(counterFactory.value(100)) // 100
-// counterFactory.decrement()
-// console.log(counterFactory.value()) // 99
-// console.log(counterFactory.value(200)) // 200
-// counterFactory.increment()
-// console.log(counterFactory.value()) // 201
+console.log(counterFactory.value()); // 0
+counterFactory.increment();
+counterFactory.increment();
+counterFactory.increment();
+console.log(counterFactory.value()); // 3
+counterFactory.decrement();
+counterFactory.decrement();
+console.log(counterFactory.value()); // 1
+console.log(counterFactory.value(100)); // 100
+counterFactory.decrement();
+console.log(counterFactory.value()); // 99
+console.log(counterFactory.value(200)); // 200
+counterFactory.increment();
+console.log(counterFactory.value()); // 201
 
 /*
  * #3
@@ -83,10 +83,10 @@ const myPow = (a, b, myPrint) => {
   return myPrint(a, b, pow(a, b));
 };
 
-// console.log(myPow(3, 4, myPrint)) // 3^4=81
-// console.log(myPow(2, 3, myPrint)) // 2^3=8
-// console.log(myPow(2, 0, myPrint)) // 2^0=1
-// console.log(myPow(2, -2, myPrint)) // 2^-2=0.25
+console.log(myPow(3, 4, myPrint)); // 3^4=81
+console.log(myPow(2, 3, myPrint)); // 2^3=8
+console.log(myPow(2, 0, myPrint)); // 2^0=1
+console.log(myPow(2, -2, myPrint)); // 2^-2=0.25
 
 /*
  * #4
@@ -95,8 +95,8 @@ const myPow = (a, b, myPrint) => {
 
 const myMax = (arr) => Math.max.apply(null, arr);
 
-// const list = [12, 23, 100, 34, 56, 9, 233]
-// console.log(myMax(list)) // 233
+const list = [12, 23, 100, 34, 56, 9, 233];
+console.log(myMax(list)); // 233
 
 /*
  * #5
@@ -108,12 +108,12 @@ const myMul = (a, b) => a * b;
 const myDouble = myMul.bind(null, 2);
 const myTriple = myMul.bind(null, 3);
 
-// console.log(myDouble(3)) // 6
-// console.log(myDouble(4)) // 8
-// console.log(myDouble(5)) // 10
+console.log(myDouble(3)); // 6
+console.log(myDouble(4)); // 8
+console.log(myDouble(5)); // 10
 
-// console.log(myTriple(3)) // 9
-// console.log(myTriple(4)) // 12
-// console.log(myTriple(5)) // 15
+console.log(myTriple(3)); // 9
+console.log(myTriple(4)); // 12
+console.log(myTriple(5)); // 15
 
 export { counter, counterFactory, myPow, myMax, myMul, myDouble, myTriple };
